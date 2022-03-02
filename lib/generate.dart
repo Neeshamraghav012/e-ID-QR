572a358e5ffe65e2d2ae9819a11a349c71e8e6ed
Neesham <neeshamraghav0@gmail.com>
2022-03-02 12:12:35 +0530

    Initial Project commit

--- lib/generate.dart
+++ lib/generate.dart
@@ -3,66 +3,184 @@ import 'package:qr_flutter/qr_flutter.dart';
 import 'dart:ui';
 import 'package:flutter/rendering.dart';
 
-class GeneratePage extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() => GeneratePageState();
-}
+class GeneratePage extends StatelessWidget {
+  var rollNo;
+  var name;
+  var avatar;
+  var qrdata;
+  GeneratePage(
+      {@required this.rollNo,
+      @required this.avatar,
+      @required this.name,
+      @required this.qrdata});
 
-class GeneratePageState extends State<GeneratePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Generate QR code'),
+        title: Text('e-ID'),
+        centerTitle: true,
+        backgroundColor: Colors.red,
       ),
-      body: ListView(
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Card(
-              elevation: 5,
-              child: ListTile(
-                leading: Icon(Icons.edit),
-                trailing: FlatButton(
-                  child: Text(
-                    "ENTER",
-                    style: TextStyle(
-                      color: Colors.white,
-                    ),
-                  ),
-                  color: Colors.green,
-                  onPressed: () {
-                    setState(() {
-                      dummyData = qrTextController.text == ""
-                          ? null
-                          : qrTextController.text;
-                    });
-                  },
-                ),
-                title: TextField(
-                  controller: qrTextController,
-                  decoration: InputDecoration(
-                    hintText: "please enter some data",
-                  ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.red,
+              ),
+              child: Text(
+                'Neesham',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
                 ),
               ),
             ),
+            ListTile(
+              leading: Icon(Icons.account_circle),
+              title: Text('Profile'),
+              onTap: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => Settings(
+                        rollNo: rollNo,
+                        name: name,
+                        avatar: avatar,
+                        qrdata: qrdata)));
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Settings'),
+              onTap: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => Settings(
+                        rollNo: rollNo,
+                        name: name,
+                        avatar: avatar,
+                        qrdata: qrdata)));
+              },
+            ),
+          ],
+        ),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Center(
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(avatar == null
+                  ? 'https://lh3.googleusercontent.com/a-/AAuE7mChgTiAe-N8ibcM3fB_qvGdl2vQ9jvjYv0iOOjB=s96-c'
+                  : avatar),
+              radius: 100,
+            ),
+          ), //CircleAvatar
+
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: SizedBox(
+              height: 50,
+              child: Text(
+                "Welcome $name",
+                style: TextStyle(
+                    fontSize: 24,
+                    color: Colors.redAccent,
+                    fontStyle: FontStyle.italic,
+                    letterSpacing: 3),
+              ),
+            ),
           ),
-          (dummyData == null)
-              ? Center(child: Text("enter some text to display qr code..."))
-              : QrImage(
-                  embeddedImage: NetworkImage(
-                    "https://avatars1.githubusercontent.com/u/41328571?s=280&v=4",
+          SizedBox(
+            child: (rollNo == null)
+                ? Center(child: Text("enter sometext to display qr code..."))
+                : QrImage(
+                    embeddedImage: NetworkImage(
+                      "https://avatas1.githubusercontent.com/u/41328571?s=280&v=4",
+                    ),
+                    data: qrdata,
+                    gapless: true,
                   ),
-                  data: dummyData,
-                  gapless: true,
-                ),
+            width: 200,
+          ),
+          SizedBox(
+            height: 50,
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            style: ElevatedButton.styleFrom(
+              primary: Colors.red,
+            ),
+            child: Text('Go Back'),
+          )
         ],
       ),
     );
   }
 }
 
-String dummyData;
+class Settings extends StatelessWidget {
+  var rollNo;
+  var name;
+  var avatar;
+  var qrdata;
 
-TextEditingController qrTextController = TextEditingController();
+  Settings(
+      {@required this.rollNo,
+      @required this.avatar,
+      @required this.name,
+      @required this.qrdata});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('e-ID'),
+        centerTitle: true,
+        backgroundColor: Colors.red,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Center(
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(avatar),
+              radius: 100,
+            ),
+          ), //CircleAvatar
+
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: SizedBox(
+              height: 50,
+              child: Text(
+                "Name: $name",
+                style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.black87,
+                    fontStyle: FontStyle.italic,
+                    letterSpacing: 3),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: SizedBox(
+              height: 50,
+              child: Text(
+                "Roll Number: $rollNo",
+                style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.black87,
+                    fontStyle: FontStyle.italic,
+                    letterSpacing: 3),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_vector_icons/flutter_vector_icons.dart';
-import 'package:qr_bar_code_flutter/scan.dart';
 import 'generate.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(MaterialApp(
@@ -16,38 +18,100 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  TextEditingController _user = TextEditingController();
+  TextEditingController _password = TextEditingController();
+
+  var username;
+  var loginpass;
+
+  var name;
+  var rollNo;
+  var avatar;
+  var qrdata;
+
+  void getData() async {
+    var credential = {
+      "user": username,
+      "password": loginpass,
+    };
+    http.Response response = await http.post(
+      Uri.parse('https://id-card-jcbose.herokuapp.com/login'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8'
+      },
+      body: jsonEncode(credential),
+    );
+
+    if (response.statusCode == 500) {
+      print("Error");
+    } else {
+      String data = response.body;
+      name = jsonDecode(data)['data']['name'];
+      avatar = jsonDecode(data)['data']['avatar'];
+      qrdata = jsonDecode(data)['data']['qrCode'];
+      rollNo = jsonDecode(data)['data']['rollNumber'];
+      print(name);
+      print(avatar);
+      print(qrdata);
+      print(rollNo);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold(
       appBar: AppBar(
-        title: Text("QR and Bar Code Reader & Generator demo"),
+        title: Text("e-ID"),
+        backgroundColor: Colors.red,
+        centerTitle: true,
       ),
-      body: ListView(
-        children: <Widget>[
-          Card(
-            child: ListTile(
-              title: Text("Scan Code"),
-              leading: Icon(MaterialCommunityIcons.qrcode_scan),
-              onTap: () {
-                Navigator.of(context)
-                    .push(MaterialPageRoute(builder: (context) => ScanPage()));
-              },
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: TextField(
+                controller: _user,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(), labelText: 'Enter Username'),
+              ),
             ),
-          ),
-          Card(
-            child: ListTile(
-              title: Text("Generate QR Code"),
-              leading: Icon(MaterialCommunityIcons.qrcode_edit),
-
-              onTap: () {
-                Navigator.of(context).push(
-                    MaterialPageRoute(builder: (context) => GeneratePage()));
-              },
+            SizedBox(
+              height: 10,
             ),
-          ),
-        ],
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: TextField(
+                controller: _password,
+                obscureText: true,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(), labelText: 'Enter Password'),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  username = _user.text;
+                  loginpass = _password.text;
+                });
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => GeneratePage(
+                          rollNo: rollNo,
+                          avatar: avatar,
+                          name: name,
+                          qrdata: qrdata,
+                        )));
+              },
+              style: ElevatedButton.styleFrom(
+                primary: Colors.red,
+              ),
+              child: Text('Log in'),
+            )
+          ],
+        ),
       ),
     );
   }
 }
-
